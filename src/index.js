a6ec3d74fa31ee712f40e3c5ed34c575ab896254
rik cousens <rcousens@colt.com>
2024-09-04 06:04:09 -0400

    tweaks; upgrade NODEJS to 12.x

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ const port = process.env.PORT || 3000;
 const hostname = '0.0.0.0';
 
 server.create()
-    .then(app => {
-        app.listen(port, hostname, () => {
-            console.log(`Server has started on port ${port}!`);
-        });
-    }).catch(err => console.log(err));
+	.then(
+		app => {
+			app.listen(port, hostname, () => {
+				console.log(`Server has started on port ${port}!`);
+			});
+		}).catch(err => console.log(err));
--- src/server.js
+++ src/server.js
@@ -6,31 +6,31 @@ const utils = require('./utils');
 // fn to create express server
 const create = async () => {
 
-    // server
-    const app = express();
-    app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
-    
-    // Log request
-    app.use(utils.appLogger);
+	// server
+	const app = express();
+	app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 
-    // root route - serve static file
-    app.get('/api/hello', (req, res) => {
-        res.json({hello: 'goodbye'});
-        res.end();
-    });
+	// Log request
+	app.use(utils.appLogger);
 
-    // root route - serve static file
-    app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/client.html')));
+	// root route - serve static file
+	app.get('/api/hello', (req, res) => {
+		res.json({ hello: 'goodbye' });
+		res.end();
+	});
 
-    // Catch errors
-    app.use(utils.logErrors);
-    app.use(utils.clientError404Handler);
-    app.use(utils.clientError500Handler);
-    app.use(utils.errorHandler);
+	// root route - serve static file
+	app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/client.html')));
 
-    return app;
+	// Catch errors
+	app.use(utils.logErrors);
+	app.use(utils.clientError404Handler);
+	app.use(utils.clientError500Handler);
+	app.use(utils.errorHandler);
+
+	return app;
 };
 
 module.exports = {
-    create
+	create
 };
